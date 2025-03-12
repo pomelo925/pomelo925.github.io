585c677c3756077d56579d270bfe416925488105
pomelo925 <yoseph.huang@gmail.com>
2025-03-12 12:22:42 +0800

    modify: vite build output dir change to build/

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build'  // 設定輸出目錄為 build
+  },
   plugins: [react()],
 })
